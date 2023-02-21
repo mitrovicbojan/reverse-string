48fe78deca0dff0d3bd66e5fa83de48af398d8b3
unknown <mitrovicbojan87@gmail.com>
2023-02-21 21:14:15 +0200

    js

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const strInput = document.getElementById("inputString");
 const btn = document.getElementById("btn");
 const reverse = document.getElementById("reverse");
+
 btn.addEventListener("click", function () {
-  console.log("hello");
-  reverse.innerHTML = strInput.value;
+  let newStr = strInput.value;
+  let revStr = "";
+  for (let i = 0; i <= newStr.length - 1; i++) {
+    revStr = newStr[i] + revStr;
+  }
+  return (reverse.innerHTML = revStr);
 });
